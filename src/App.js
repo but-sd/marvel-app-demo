668daaa99e8bd45127b051b2326f9c172f662cc7
Alexandre GIRARD <alexandre.girard@maif.fr>
2023-10-27 15:01:36 +0200

    Transform characters list page into react component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import './style.css';
+
+const characters = require('./data/characters.json');
 
 function App() {
+  console.log((characters));
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Marvel Characters</h1>
+      <ul id="characters">
+        {characters.map((character) => (
+          <li key={character.id}>{character.name}</li>
+        ))}
+      </ul>
+    </>
   );
 }
 
